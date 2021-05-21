c2fb95d8cee40147e6c2b567b28c44fb624bb669
Sachin D. Shinde <sachin@apollographql.com>
2021-05-21 13:59:22 -0700

    Move queryPlanStore.flush() to inside updateWithSchemaAndNotify()

--- gateway-js/src/index.ts
+++ gateway-js/src/index.ts
@@ -467,8 +467,6 @@ export class ApolloGateway implements GraphQLService {
     this.compositionMetadata = result.compositionMetadata;
     this.serviceDefinitions = result.serviceDefinitions;
 
-    if (this.queryPlanStore) this.queryPlanStore.flush();
-
     const { schema, supergraphSdl } = this.createSchemaFromServiceList(
       result.serviceDefinitions,
     );
@@ -530,8 +528,6 @@ export class ApolloGateway implements GraphQLService {
     this.compositionId = result.id;
     this.parsedSupergraphSdl = parsedSupergraphSdl;
 
-    if (this.queryPlanStore) this.queryPlanStore.flush();
-
     const { schema, supergraphSdl } = this.createSchemaFromSupergraphSdl(
       result.supergraphSdl,
     );
@@ -568,6 +564,7 @@ export class ApolloGateway implements GraphQLService {
     // Once we remove the deprecated onSchemaChange() method, we can remove this.
     legacyDontNotifyOnSchemaChangeListeners: boolean = false,
   ): void {
+    if (this.queryPlanStore) this.queryPlanStore.flush();
     this.schema = schema;
     this.queryPlanner = new QueryPlanner(schema);
 
